e86d66b17c13c13cd10f6b616536a54b7e3431e8
satyavan2396 <satyavan2396@gmail.com>
2024-01-24 19:35:29 +0530

    24-jan assignment completed
    24-jan assignment completed

---
+++ Dom/14_main.js
@@ -0,0 +1,8 @@
+const profiledetails=document.querySelector("#profile");
+console.log(profiledetails.innerHTML);
+profiledetails.style.color="green";
+
+const technologyStack=document.querySelector(".tech")
+console.log(technologyStack);
+technologyStack.style.fontSize = 'large';
+technologyStack.style.fontWeight = 'bold';
